6601ec94fa3a8b4508e6a6c40c2e5bbaf0f49a2e
Besik Kelekhsaevi <158075799+subzero1221@users.noreply.github.com>
2026-08-17 23:54:15 +0400

    Fix formatting in Stripe route.js

--- app/api/payments/stripe/route.js
+++ app/api/payments/stripe/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
+ 
 export async function POST(req) {
   try {
     const { shopId, plan, userId } = await req.json();
